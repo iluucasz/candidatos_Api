1c28a64a9dde5ede3473f097a7cf671c502dedb3
Hyven <lucasface99@gmail.com>
2024-02-23 11:17:14 -0300

    cors add

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import express, { json } from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import { opportunityRouter } from './routers/opportunity.route';
 import { HandleErrors } from './middlewares/handleErrors.middleware';
 import { userRouter } from './routers/users.route';
 
 export const app = express();
 app.use(helmet());
+app.use(cors());
 app.use(json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/opportunities', opportunityRouter);
